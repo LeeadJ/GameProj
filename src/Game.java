3f02085983092b7d568599e6d824c8fe416935aa
LeeadJ <leead123@gmail.com>
2022-08-18 18:03:26 +0300

    added window configuration

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,23 @@
-public class Game implements Runnable{
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+    private static final long serialVersionUID = 1L;
+
     public static int width = 300;
     public static int height = width / 16 * 9;
     public static int scale = 3; //scaling the game to x3 the game size, but with performance of 300 pixels wide.
 
     private Thread thread;
+    private JFrame frame;
     private boolean running = false;
 
+    public Game(){
+        Dimension size = new Dimension(width*scale, height*scale);
+        setPreferredSize(size); //Canvas method
+
+        frame = new JFrame(); //creating a new instance of JFrame when creating a new Game.
+    }
     //synchronized = prevents thread interferences and memory consistency errors
     public synchronized void start(){
         running = true; //game will loop without stopping.
@@ -24,8 +36,22 @@ public class Game implements Runnable{
     //run method will contain the game loop method, making sure we won't exit out of the game:
     public void run(){
         while(running){ //while running is true, game will work.
-
+            System.out.println("Running....");
         }
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false); //making sure our window won't be resized. (causes graphical errors)
+        game.frame.setTitle("Rain");
+        game.frame.add(game); //adding a game component to our window.
+        game.frame.pack(); //sets the size of the frame to the size of the component.
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the game program when 'x' is clicked in the top-right corner.
+        game.frame.setLocationRelativeTo(null); //centers the frame into the screen.
+        game.frame.setVisible(true); //makes sure the game window is visible and not hidden.
+
+        game.start();
+
     }
 
 }
