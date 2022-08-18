95df3c96a3bdc0e3b1be466012048c2da6c1dd6c
LeeadJ <leead123@gmail.com>
2022-08-18 17:43:07 +0300

    added running for Game Loop

--- src/Game.java
+++ src/Game.java
@@ -4,18 +4,27 @@ public class Game implements Runnable{
     public static int scale = 3; //scaling the game to x3 the game size, but with performance of 300 pixels wide.
 
     private Thread thread;
+    private boolean running = false;
 
     //synchronized = prevents thread interferences and memory consistency errors
     public synchronized void start(){
+        running = true; //game will loop without stopping.
         thread = new Thread(this, "Display");
         thread.start();
     }
     //making sure when game is closed the thread is properly shut down:
     public synchronized void stop(){
+        running = false; //game loop will stop.
         try{
             thread.join();
         } catch(InterruptedException e){
             e.printStackTrace(); //exit out of the program.
+        }
+    }
+    //run method will contain the game loop method, making sure we won't exit out of the game:
+    public void run(){
+        while(running){ //while running is true, game will work.
+
         }
     }
 
